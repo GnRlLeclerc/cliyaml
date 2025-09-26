1c346fcd84f692924afba4e3110c99c09841f77e
Thibaut <thibaut2saivre@gmail.com>
2025-09-26 15:02:46 +0200

    add automatic shorthands and prefix shorthands

--- src/cliyaml/cli.py
+++ src/cliyaml/cli.py
@@ -12,12 +12,20 @@ def build(tree: Tree, description: str = "") -> argparse.ArgumentParser:
     return parser
 
 
-def add_to_parser(parser: argparse.ArgumentParser, tree: Tree, prefix=""):
+def add_to_parser(
+    parser: argparse.ArgumentParser, tree: Tree, prefix="", prefix_shorthand=""
+):
     """Recursively traverse the tree and add arguments to the parser."""
+
+    prefix_shorthands = set()
+    shorthands = set()
 
     for key, value in tree.items():
         if isinstance(value.value, dict):
-            add_to_parser(parser, value.value, f"{prefix}{key}-")
+            shortand = get_shorthand(key, prefix_shorthands)
+            add_to_parser(
+                parser, value.value, f"{prefix}{key}-", f"{prefix_shorthand}{shortand}-"
+            )
             continue
 
         args: dict = {
@@ -31,4 +39,17 @@ def add_to_parser(parser: argparse.ArgumentParser, tree: Tree, prefix=""):
         if value.value is not None:
             args["default"] = value.value
 
-        parser.add_argument(f"--{prefix}{key}", **args)
+        shorthand = get_shorthand(key, shorthands)
+
+        parser.add_argument(
+            f"-{prefix_shorthand}{shorthand}", f"--{prefix}{key}", **args
+        )
+
+
+def get_shorthand(key: str, used: set[str]) -> str:
+    """Get a unique shorthand for a key."""
+    i = 1
+    while (shorthand := key[0:i]) in used:
+        i += 1
+    used.add(shorthand)
+    return shorthand
